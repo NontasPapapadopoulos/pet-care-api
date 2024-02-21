2f43e45d0b74add80578fe43707471d502be2b31
Nontas <nodis92@hotmail.com>
2024-02-21 08:26:19 -0800

    add staff

---
+++ src/main/java/nondas/pap/petcare/controller/AuthenticationController.java
@@ -0,0 +1,40 @@
+package nondas.pap.petcare.controller;
+
+import lombok.RequiredArgsConstructor;
+import nondas.pap.petcare.service.AuthenticationService;
+import nondas.pap.petcare.model.AuthenticationRequest;
+import nondas.pap.petcare.model.AuthenticationResponse;
+import nondas.pap.petcare.model.RegisterRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+
+    
+
+}
---
+++ src/main/java/nondas/pap/petcare/service/AuthenticationService.java
@@ -0,0 +1,64 @@
+package nondas.pap.petcare.service;
+
+
+import lombok.RequiredArgsConstructor;
+import nondas.pap.petcare.config.JwtService;
+import nondas.pap.petcare.model.AuthenticationRequest;
+import nondas.pap.petcare.model.AuthenticationResponse;
+import nondas.pap.petcare.model.RegisterRequest;
+import nondas.pap.petcare.model.User;
+import nondas.pap.petcare.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .email(request.getEmail())
+                .name(request.getName())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+}
